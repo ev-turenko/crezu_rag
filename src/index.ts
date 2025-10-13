72a17c4644a917d4389c787d60d4623a132a8a4f
Evgenii T <johnnitzevans@yahoo.com>
2025-10-13 08:36:45 -0600

    fixes

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import healthcheckRoutes from './routes/healthcheckRoutes.js';
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
