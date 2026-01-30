0945a562567b8266b74c6ba28a08b3a9b238c8f4
Evgenii Turenko <evgeniiturenko@192.168.1.47>
2026-01-29 21:56:42 -0600

    updated config

--- src/controllers/configController.ts
+++ src/controllers/configController.ts
@@ -2,16 +2,19 @@ import { Request, Response } from 'express';
 
 export function getConfig() {
     return async (req: Request, res: Response) => {
-        // const app = req.query.app as string | undefined;
-        // const platform = req.query.platform as string | undefined;
+        const appName = req.query.app_name as string | undefined;
+        const appVersion = req.query.app_version as string | undefined;
+        const appBuildNumber = req.query.app_build_number as string | undefined;
+        const platform = req.query.platform as string | undefined;
         return res.json({
-            version: '1.0.0',
+            version: appBuildNumber,
             supportedLanguages: ['en', 'es', 'pl'],
             regScreens: ['auth1', 'auth2', 'auth3', 'auth4', 'auth5'],
             regScreensPolicy: "enforce", // enforce | optional | disabled
             feedEndpoint: 'https://ai.cashium.pro/api/offer',
             inferenceEndpoint: 'https://ai.cashium.pro/api/inference',
             authEndpoint: 'https://auth.cashium.pro/api/v1/auth',
+            dataEndpoint: 'https://data.cashium.pro/api/v1/data',
         })
     }
 }
