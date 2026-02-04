02c3c3cd136c7f5b214eb194aa76d859aa662e45
Evgenii Turenko <evgeniiturenko@192.168.1.3>
2026-02-04 00:27:20 -0600

    changes

--- src/controllers/configController.ts
+++ src/controllers/configController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 export function getConfig() {
     return async (req: Request, res: Response) => {
@@ -6,7 +7,13 @@ export function getConfig() {
         const appVersion = req.query.app_version as string | undefined;
         const appBuildNumber = req.query.app_build_number as string | undefined;
         const platform = req.query.platform as string | undefined;
+        let client_id = req.query.client_id as string | undefined;
+        if(!client_id) {
+            // generate v4 uuid
+            client_id = uuidv4();
+        }
         return res.json({
+            client_id: client_id,
             version: appBuildNumber,
             supportedLanguages: ['en', 'es', 'pl'],
             regScreens: ['auth1', 'auth2', 'auth3', 'auth4', 'auth5'],
