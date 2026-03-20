a39ee1983c66a0bb3d834997efec16eac799426e
Evgenii Turenko <evgeniiturenko@MacBook-Pro-Evgenii-2.local>
2026-03-20 00:34:34 -0600

    changes

--- src/controllers/chatGreetingController.ts
+++ src/controllers/chatGreetingController.ts
@@ -1,5 +1,15 @@
 import { Request, Response } from 'express';
 
 export const getChatGreeting = (_: Request, res: Response) => {
-    res.status(200).json(null);
+    res.status(200).json({
+        "welcome_message": "ABC",
+        "features": [
+            "123",
+            "456"
+        ],
+        "anon_welcome_message": "You're in private mode now",
+        "anon_features": [
+            "AI assistant won't remember this conversation. It will be deleted automatically once you close the chat."
+        ]
+    });
 };
