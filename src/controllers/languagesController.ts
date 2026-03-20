d0fded50d6c15ca87862b511a81be245d2f9fa37
Evgenii Turenko <evgeniiturenko@MacBook-Pro-Evgenii-2.local>
2026-03-19 22:58:46 -0600

    updated code

--- src/controllers/languagesController.ts
+++ src/controllers/languagesController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { COUNTRIES_CONFIG } from './countriesController.js';
 
 export function getLanguages() {
     return async (_req: Request, res: Response) => {
@@ -10,12 +11,7 @@ export function getLanguages() {
                 { code: 'pl' },
                 { code: 'sv' }
             ],
-            countries: [
-                { provider: '374', country_code: 'es', country_id: '1' },
-                { provider: '373', country_code: 'mx', country_id: '2' },
-                { provider: '375', country_code: 'pl', country_id: '14' },
-                { provider: '377', country_code: 'se', country_id: '22' }
-            ]
+            countries: [...COUNTRIES_CONFIG]
         });
     };
 }
