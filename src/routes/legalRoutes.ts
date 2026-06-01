032da4ad6dfd4a23349a9a76f8e01719fe37a99e
Evgenii Turenko <evgeniiturenko@MacBook-Pro-Evgenii-2.local>
2026-05-31 22:58:39 -0600

    fixed config for legal routes

--- src/routes/legalRoutes.ts
+++ src/routes/legalRoutes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { getPrivacyPolicy } from '../controllers/legalController.js';
+import { getTermsAndConditions } from '../controllers/termsController.js';
 
 const router = Router();
 router.get('/:code/privacy-policy', getPrivacyPolicy());
+router.get('/:code/terms-and-conditions', getTermsAndConditions());
 export default router;
